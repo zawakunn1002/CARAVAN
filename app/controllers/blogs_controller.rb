8547fc0191e398abb4e57bf77cfbd81fea6c0e54
daichi <bumpk1002@gmail.com>
2019-05-11 21:21:37 +0900

    [Finish]4-3

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,25 @@
+class BlogsController < ApplicationController
+  def show
+  end
+
+  def index
+  end
+
+  def new
+  	 @blog = Blog.new
+  end
+
+  def create
+  	  blog = Blog.new(blog_params)
+  	  blog.save
+  	  redirect_to blogs_path
+  end
+
+  def edit
+  end
+
+  private
+  def blog_params
+  	parmas.require(:blog).permit(:title, :category, :body)
+  end
+end
--- db/migrate/20190511103457_create_blogs.rb
+++
@@ -1,8 +0,0 @@
-class CreateBlogs < ActiveRecord::Migration[5.2]
-  def change
-    create_table :blogs do |t|
-
-      t.timestamps
-    end
-  end
-end
