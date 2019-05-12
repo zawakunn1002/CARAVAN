275fcdbb6e294fdd117f3deb37a54fe94d455235
daichi <bumpk1002@gmail.com>
2019-05-12 12:20:04 +0900

    [Finish]4-6

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,8 +1,11 @@
 class BlogsController < ApplicationController
   def show
+  	 @blog = Blog.find(params[:id])
   end
 
   def index
+
+  	 @blogs = Blog.all
   end
 
   def new
@@ -16,10 +19,17 @@ class BlogsController < ApplicationController
   end
 
   def edit
+  	  @blog = Blog.find(params[:id])
+  end
+
+  def update
+  	  blog = Blog.find(params[:id])
+  	  blog.update(blog_params)
+  	  redirect_to blog_path(blog)
   end
 
   private
   def blog_params
-  	parmas.require(:blog).permit(:title, :category, :body)
+  	params.require(:blog).permit(:title, :category, :body)
   end
 end
